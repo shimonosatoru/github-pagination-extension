0738faf9c0a7638eeacd4627a7dabe2e97b3583a
shimonosatoru <michael513.sato@gmail.com>
2019-06-11 16:47:01 +0900

    Remove buildSiblingsButton method

--- src/js/contents.js
+++ src/js/contents.js
@@ -111,41 +111,22 @@ $(function() {
         } else if (pageStatus === 1) {
 
             // NewerのHTMLを保存しておく
+            const newerHTML = pageBtnElem;
 
             // NewerとOlderを挿入する
-            buildSiblingsButton(true, false, pageNum);
+            pageGroupElem.prepend(newerHTML);
+            pageGroupElem.append('<button class="btn btn-outline BtnGroup-item" disabled="disabled">Older</button>');
             
         } else if (pageStatus === 2) {
 
             // NewerとOlderのHTMLを保存しておく
+            const newerHTML = pageBtnElem.first();
+            const olderHTML = pageBtnElem.first().next();
 
             // NewerとOlderを挿入する
-            buildSiblingsButton(true, true, pageNum);
+            pageGroupElem.prepend(newerHTML);
+            pageGroupElem.append(olderHTML);
 
-        }
-    }
-
-    /**
-     * buildSiblingsButton
-     * NewerまたはOlderが押せるか押せないかの2つを引数にとる
-     * @param {*} hasNewer 
-     * @param {*} hasOlder 
-     */
-    function buildSiblingsButton(hasNewer, hasOlder, pageNum) {
-
-        if (hasNewer) {
-            pageGroupElem.prepend('<a rel="nofollow" class="btn btn-outline BtnGroup-item btn-newer" ' +
-                                  'href="https://github.com/ruby/ruby/commits/trunk?after=140b8117bd3c32cb9d0b144937b90f0178a00b0e+' +
-                                  (pageNum - 1) * 35 + '">Newer</a>');
-                                  href="https://github.com/ruby/ruby/commits/trunk?before=140b8117bd3c32cb9d0b144937b90f0178a00b0e+35"
-        } else {
-            pageGroupElem.prepend('<button class="btn btn-outline BtnGroup-item" disabled="disabled">Newer</button>')
-        }
-
-        if (hasOlder) {
-            pageGroupElem.append('<a rel="nofollow" class="btn btn-outline BtnGroup-item btn-older" href="https://github.com/ruby/ruby/commits/trunk?after=140b8117bd3c32cb9d0b144937b90f0178a00b0e+69">Older</a>');
-        } else {
-            pageGroupElem.append('<button class="btn btn-outline BtnGroup-item" disabled="disabled">Older</button>')
         }
     }
 });
